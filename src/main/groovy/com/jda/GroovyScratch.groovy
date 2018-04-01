0d96d3bca6b74202c87ed62f0ff20a53af63c417
Juan Alvarado <juaalvarado@ebay.com>
2018-04-01 08:13:19 -0700

    Initial commit to push to github

---
+++ src/main/groovy/com/jda/GroovyScratch.groovy
@@ -0,0 +1,16 @@
+package com.jda
+
+class GroovyScratch {
+
+    static void main(String[] args) {
+        def strings = ['this','is','a','list','of','strings']
+        Collections.sort(strings, {s1,s2 -> s2.size() - s1.size()} as Comparator)
+        assert strings*.size() == [7, 4, 4, 2, 2, 1]
+
+        print(strings)
+
+        Collections.sort(strings);
+        print("\n")
+        print(strings)
+    }
+}
---
+++ src/main/java/com/jda/SampleLogging.java
@@ -0,0 +1,17 @@
+package com.jda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleLogging {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SampleLogging.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            if (i % 2 == 0)
+                LOGGER.info("Hello {}", i);
+            else
+                LOGGER.debug("I am on index {}", i);
+    }
+}
